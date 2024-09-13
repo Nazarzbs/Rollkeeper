f94b1dff6f5e149cbf6a06df5ddce1fda9d41599
Nazar <famina1998@gmail.com>
2024-09-13 21:31:50 +0300

    Initial Commit

---
+++ Rollkeeper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rollkeeper
+//
+//  Created by Nazar on 13/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rollkeeper/RollkeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollkeeperApp.swift
+//  Rollkeeper
+//
+//  Created by Nazar on 13/9/24.
+//
+
+import SwiftUI
+
+@main
+struct RollkeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
